57ee0536331a71529d4318b0212de58c7d3e34d7
Victoria Stovba <vstovba.00@gmail.com>
2025-01-19 01:08:53 +0400

    home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,13 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Campers of your dreams</h1>
-      <p>You can find everything you want in our catalog</p>
-      <Link to="/catalog" className={styles.button}>
-        View Now
-      </Link>
+      <div className={styles.textArea}>
+        <h1>Campers of your dreams</h1>
+        <p>You can find everything you want in our catalog</p>
+        <Link to="/catalog" className={styles.button}>
+          View Now
+        </Link>
+      </div>
     </div>
   );
 };
